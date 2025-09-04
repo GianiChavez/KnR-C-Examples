812712683e4c29ff10c1ce3e9dfc3d625a11c204
Giani Chavez <gianichavez09@gmail.com>
2025-09-04 16:39:29 -0400

    Added VarCelsTable.c & compiled .exe

--- .vscode/1.2.FahrCelsTable/VarCelsTable.c
+++ .vscode/1.2.FahrCelsTable/VarCelsTable.c
@@ -8,16 +8,21 @@
 
 int main()
 {
-    int F, C;                   // Define Vars
-    int lower, upper, step;     
+    float F, C;                         // Define Vars
+    float lower, upper, step;     
 
-    lower = 0;                  // Define lower, upper, & step bounds
+    lower = 0;                          // Define lower, upper, & step bounds
     upper = 300;
     step = 20;
 
-    F = lower;                  // Initialize F to lower bound
-    while (F <= lower) {        
-        C = 5 * (F-32) / 9;         // Calculation for C
-        printf("%d\t%d\n", F, C);   // Print Table. %d is a placeholder for an int arguments
-    }
+    printf("Temp (F)\tTemp (C)\n");         // Print Header. \t is a tab. \n is newline.
+    F = lower;                              // Initialize F to lower bound
+    
+    while (F <= upper) {        
+        C = (5.0/9.0) * (F-32.0);           // Calculation for C
+        printf("%3.0f\t \t%6.1f\n", F, C);  // Print Table. %d is a placeholder for an int arguments
+        F = F + step;                       // The number inside %d is the width of the line and forces a right-justify
+                                            // In the 2nd version, we replace with %f to signify a float. Each digit
+    }                                       // indicates precision on either side of the decimal point.
+                                    
 }
